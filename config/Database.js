0d28f7aadb03113c596d30f005459e601ef97d9f
arifwdn <muhammadarifwardana01@gmail.com>
2023-01-10 12:27:02 +0800

    fixed with functional

--- config/Database.js
+++ config/Database.js
@@ -1,47 +1,37 @@
 const oracledb = require("oracledb");
+const setting = require("./setting-db");
 oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
-class db {
-  constructor(user, password, hostDB) {
-    this.user = user;
-    this.password = password;
-    this.host = hostDB;
-  }
-  connect(usr, pss, hostdb) {
-    return oracledb.getConnection({
-      user: usr,
-      password: pss,
-      connectString: hostdb,
-    });
-  }
-  query(query, bind = [], options = {}) {
-    return new Promise(async function (resolve, reject) {
-      let connection;
-      try {
-        connection = await this.connect(this.user, this.password, this.hostDB);
-        const result = await connection.execute(query, bind, options);
-        resolve(result);
-      } catch (err) {
-        reject(err);
-      } finally {
-        if (connection) {
-          try {
-            await connection.close();
-          } catch (e) {
-            console.error(e);
-          }
+const query = (q, bind = [], options = {}) => {
+  return new Promise(async function (resolve, reject) {
+    let connection;
+    try {
+      connection = await oracledb.getConnection({
+        user: setting.user,
+        password: setting.password,
+        connectString: setting.hostDB,
+      });
+      const result = await connection.execute(q, bind, options);
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    } finally {
+      if (connection) {
+        try {
+          await connection.release();
+        } catch (e) {
+          console.error(e);
         }
       }
-    });
-  }
-  async eksekusi(q = { query, bind, options }, func = (result) => result) {
-    try {
-      let result = await this.query(q.query, q.bind, q.options);
-      func(result);
-    } catch (err) {
-      console.error(err);
     }
+  });
+};
+const eksekusi = async (q, func = (result) => result) => {
+  try {
+    result = await query(q.query, q.bind, q.options);
+    func(result);
+  } catch (err) {
+    console.error(err);
   }
-}
-
-module.exports = db;
+};
+module.exports = eksekusi;
