17ed35a6fb0853f2543586f314a1da2edef5d3d3
arifwdn <muhammadarifwardana01@gmail.com>
2023-01-10 10:04:11 +0800

    Testing

--- config/setting-db.js
+++ config/setting-db.js
@@ -1,6 +1,6 @@
 // fill this data DB or can set with env
 const dataDB = {
-  user: "username", //Username Database
+  user: "dnwandkjnadkn anwo", //Username Database
   password: "pw", //Password Database
   hostDB: "host/database", //Host / database name
 };
